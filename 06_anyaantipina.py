d432377ea64d9ad51b6b14f31bb11565deddca3d
Anna Antipina <anya.antipina@gmail.com>
2019-04-09 15:38:09 +0300

    The clean buttons added

--- 06_anyaantipina.py
+++ 06_anyaantipina.py
@@ -81,7 +81,13 @@ class MyApp(App):
         else:
             for item in self.Canvas2.find_all():
                 self.Canvas1.create_line((*self.Canvas2.coords(item)), fill = self.Canvas2.itemcget(item, "fill"))
-    
+
+    def clean(self, event, index):
+        if (index == 1):
+            self.Canvas1.delete(ALL)
+        else:
+            self.Canvas2.delete(ALL)
+
     def create(self):
         self.Canvas1 = Paint(self, foreground="midnightblue")
         self.Canvas1.grid(row=0, column=0, sticky=N+E+S+W)
@@ -99,13 +105,19 @@ class MyApp(App):
         self.ShowColor = Label(frame, textvariable=self.Canvas1.foreground, bg = "midnightblue", fg = "white")
         self.ShowColor.grid(row=1, column=0, sticky=N+W+E)
         self.Quit = Button(frame, text="Quit", command=self.quit)
-        self.Quit.grid(row=4, column=0, sticky=N+W)
+        self.Quit.grid(row=6, column=0, sticky=N+W)
         self.Copy1_2 = Button(frame, text="Copy from 1 to 2")
         self.Copy1_2.bind("<Button-1>", lambda event: self.copy(event, 1))
         self.Copy1_2.grid(row=2, column=0, sticky=N+W)
         self.Copy2_1 = Button(frame, text="Copy from 2 to 1")
         self.Copy2_1.bind("<Button-1>", lambda event: self.copy(event, 2))
         self.Copy2_1.grid(row=3, column=0, sticky=N+W)
+        self.Clean1 = Button(frame, text="Clean 1")
+        self.Clean1.bind("<Button-1>", lambda event: self.clean(event, 1))
+        self.Clean1.grid(row=4, column=0, sticky=N+W)
+        self.Clean2 = Button(frame, text="Clean 2")
+        self.Clean2.bind("<Button-1>", lambda event: self.clean(event, 2))
+        self.Clean2.grid(row=5, column=0, sticky=N+W)
 
 app = MyApp(Title="Canvas Example")
 app.mainloop()
